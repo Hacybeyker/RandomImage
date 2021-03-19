3aadda50ea13b2daef27beb2ba74ce6dc6b838d4
Carlos Osorio <hacybeyker@gmail.com>
2021-03-19 03:30:06 -0500

    was implemented paging source and data

--- app/src/main/java/com/hacybeyker/randomimage/repository/UnsplashRepository.kt
+++ app/src/main/java/com/hacybeyker/randomimage/repository/UnsplashRepository.kt
@@ -1,5 +1,8 @@
 package com.hacybeyker.randomimage.repository
 
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.liveData
 import com.hacybeyker.randomimage.api.UnsplashApi
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -10,6 +13,18 @@ import javax.inject.Singleton
 
 @Singleton
 class UnsplashRepository @Inject constructor(private val unsplashApi: UnsplashApi) {
+
+    fun getSearchResults(query: String) =
+        Pager(
+            config = PagingConfig(
+                pageSize = 20,
+                maxSize = 100,
+                enablePlaceholders = false
+            ),
+            pagingSourceFactory = {
+                UnsplashPagingSource(unsplashApi, query)
+            }
+        ).liveData
 
 
 }
--- app/src/main/java/com/hacybeyker/randomimage/ui/gallery/GalleryViewModel.kt
+++ app/src/main/java/com/hacybeyker/randomimage/ui/gallery/GalleryViewModel.kt
@@ -1,6 +1,10 @@
 package com.hacybeyker.randomimage.ui.gallery
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.switchMap
+import androidx.lifecycle.viewModelScope
+import androidx.paging.cachedIn
 import com.hacybeyker.randomimage.repository.UnsplashRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -12,4 +16,18 @@ import javax.inject.Inject
 @HiltViewModel
 class GalleryViewModel @Inject constructor(private val repository: UnsplashRepository) :
     ViewModel() {
+
+    private val currentQuery = MutableLiveData(DEFAULT_QUERY)
+
+    val photos = currentQuery.switchMap { queryString ->
+        repository.getSearchResults(query = queryString).cachedIn(viewModelScope)
+    }
+
+    fun searchPhotos(query: String) {
+        currentQuery.value = query
+    }
+
+    companion object {
+        private const val DEFAULT_QUERY = "dogs"
+    }
 }
